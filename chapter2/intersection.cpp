b39e62f2737fc9e9a34221ccb97ffb872a51eebd
Marc <marc82490@gmail.com>
2021-05-17 07:23:32 -0500

    Complete solution for Intersection

--- chapter2/intersection.cpp
+++ chapter2/intersection.cpp
@@ -18,6 +18,7 @@ class LinkedList {
         void display();
         Node *getHead() { return head; }
         void setHead(Node *node) { head = node; }
+        int getLen();
 
     private:
         Node* head{};
@@ -33,6 +34,20 @@ void LinkedList::addVal(int val) {
 
     newNode->setNext(head);
     head = newNode;
+}
+
+int LinkedList::getLen() {
+    if (!head) {
+        return 0;
+    }
+
+    int count{};
+    Node *curr = head;
+    while (curr != nullptr) {
+        curr = curr->getNext();
+        count++;
+    }
+    return count;
 }
 
 void LinkedList::display() {
@@ -66,7 +81,38 @@ LinkedList addToList(LinkedList list, int val) {
 }
 
 Node* isIntersecting(LinkedList list1, LinkedList list2) {
-    Node* 
+    Node *list1Node = list1.getHead();
+    Node *list2Node = list2.getHead();
+
+    int len1 = list1.getLen();
+    int len2 = list2.getLen();
+    int diff = len1 - len2;
+
+    if (diff) {
+        int maxLen = len1 > len2 ? len1 : len2;
+        int minLen = len1 < len2 ? len1 : len2;
+        diff = maxLen - minLen;
+    }
+    
+    while (diff) {
+        if (len1 > len2) {
+            list1Node = list1Node->getNext();
+        } else {
+            list2Node = list2Node->getNext();
+        }
+        diff--;
+    }
+
+    while (list1Node != nullptr || list2Node != nullptr) {
+        if (list1Node == list2Node) {
+            return list1Node;
+        } else {
+            list1Node = list1Node->getNext();
+            list2Node = list2Node->getNext();
+        }
+    }
+
+    return NULL;
 }
 
 int main() {
@@ -83,9 +129,10 @@ int main() {
     linkedList2.display();
 
     LinkedList newList = addToList(linkedList1, 10);
+    newList = addToList(newList, 11);
 
     newList.display();
 
-    std::cout << isIntersecting(linkedList1, newList);
-    std::cout << isIntersecting(linkedList2, newList);
+    std::cout << isIntersecting(linkedList1, newList) << '\n';
+    std::cout << isIntersecting(linkedList2, newList) << '\n';
 }
